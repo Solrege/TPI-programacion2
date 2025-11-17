bde31713955cb0007135bf06baee66c9e9ad6f7c
unknown <ortelladokaren@gmail.com>
2025-11-16 23:57:59 -0300

    agrego archivo de prueba de conexión

---
+++ trabajo_integrador/src/trabajo_integrador/main/TestConnection.java
@@ -0,0 +1,26 @@
+package trabajo_integrador.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import trabajo_integrador.config.DatabaseConnection;
+
+
+public class TestConnection {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Intentando conectar a la base de datos...");
+        
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            
+            if (conn != null) {
+                System.out.println("¡Conexión a la BBDD exitosa!");
+                System.out.println("AutoCommit: " + conn.getAutoCommit());
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(" Error en la conexión");
+        }
+    }
+
+}
